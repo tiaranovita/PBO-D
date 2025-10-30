798c7743c8ee5061c4f53b3d51ce5e7f9ef5cf37
TiaraNovitaSari <if24.tiarasari@mhs.ubpkarawang.ac.id>
2025-10-30 16:49:36 +0700

    ini pakai constructor

--- lingkaran.php
+++ lingkaran.php
@@ -1,9 +1,13 @@
 <?php
-
+// Ini versi constructor
 class LuasLingkaran{
 
     public const phi = 3.14;
     public int $jari;
+
+    public function __construct($isijari = 1) {
+        $this->jari = $isijari;
+    }
 
     public function tampil($nama ='ban')  {
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
@@ -14,9 +18,13 @@ class LuasLingkaran{
         echo "<br/>";
         echo "ini dari static";
     }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 7;
+$lingkaran = new LuasLingkaran(7);
 $lingkaran->tampil('roda'); //panggil method
 LuasLingkaran::testing();
